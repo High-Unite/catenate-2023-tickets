97327e51790dba0b5001270a2b10fb4ec6c2044f
emendoza2 <elijahtanchimendoza@gmail.com>
2022-07-22 22:09:38 +0800

    Ooh baby give me one more chance

---
+++ functions/_lib/renderTemplate.js
@@ -0,0 +1,119 @@
+const QRCode = require('qrcode')
+
+module.exports = async function renderTemplate({ id, name }) {
+    const qrDataUrl = await QRCode.toDataURL(
+        JSON.stringify({ id, name })
+      )
+  return `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <title>Catenate Ticket Template</title>
+
+    <style>
+        :root {
+        --color-bg: #69f7be;
+        --color-text-main: #000000;
+        --color-primary: #ffff00;
+        --wrapper-height: 100vh;
+        --font-family: "Albert Sans", -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+        --font-family-header: "Albert Sans", -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+        }
+
+        /* Basic page style resets */
+        * {
+        box-sizing: border-box;
+        }
+        [hidden] {
+        display: none !important;
+        }
+
+        body,
+        html {
+        margin: 0;
+        padding: 0;
+        }
+
+        body {
+        font-family: var(--font-family);
+        background-color: var(--color-bg);
+        }
+
+        /* Page structure */
+        .wrapper {
+        min-height: var(--wrapper-height);
+        display: flex;
+        flex-direction: column;
+        }
+
+        .content {
+        padding: 0 4em;
+        flex: 1;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        }
+
+        .footer {
+        padding: 1em;
+        background-color: white;
+        display: flex;
+    justify-content: space-between;
+        }
+        .person {
+            padding-right: 2em;
+        }
+        
+        .person__name {
+        font-size: 2.5rem;
+        font-weight: bold;
+        }
+        
+        .person__id {
+        font-size: 1.5rem;
+        margin-top: 0.5rem;
+        }
+
+        .qr-code__image {
+        border-radius: 1em;
+        overflow: hidden;
+        }
+    </style>
+    </head>
+    <body>
+    <div class="wrapper">
+        <main class="content">
+        <div class="person">
+            <div class="person__name name">
+            ${name}
+            </div>
+            ${
+              id
+                ? `
+            <div class="person__id id">
+            ${id}
+            </div>
+            `
+                : ``
+            }
+        </div>
+        <div class="qr-code">
+            <img
+            class="qr-code__image"
+            src="${qrDataUrl}"
+            />
+        </div>
+        </main>
+        <footer class="footer">
+        <div class="footer__logo logo">CATENATE</div>
+        <div class="footer__admit-one">Admit One</div>
+        <div class="footer__date date">August 29, 2020</div>
+        </footer>
+    </div>
+    </body>
+</html>`;
+};
---
+++ functions/_lib/screenshot.js
@@ -0,0 +1,101 @@
+const chromium = require("chrome-aws-lambda");
+
+module.exports = async function screenshot(
+  html,
+  { format, viewport, dpr = 1, withJs = false, wait, timeout = 60*1000 /* 8500 */ }
+) {
+  // Must be between 3000 and 8500
+  timeout = Math.min(Math.max(timeout, 3000), 60*1000);
+
+  const browser = await chromium.puppeteer.launch({
+    executablePath:
+      process.env.CHROME_EXE_PATH || (await chromium.executablePath),
+    args: chromium.args,
+    defaultViewport: {
+      width: viewport[0],
+      height: viewport[1],
+      deviceScaleFactor: parseFloat(dpr),
+    },
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+
+  if (!withJs) {
+    page.setJavaScriptEnabled(false);
+  }
+
+  let response = await Promise.race([
+    await page.setContent(html, { waitUntil: "domcontentloaded", timeout }),
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(false); // false is expected below
+      }, timeout - 1500); // we need time to execute the window.stop before the top level timeout hits
+    }),
+  ]);
+  //   if (!wait) {
+  //       await Promise.race([
+  //         page.setContent(html, {
+  //           waitUntil: wait || ["load"],
+  //           timeout,
+  //         }),
+  //         new Promise((resolve) => {
+  //           setTimeout(() => {
+  //             resolve(false); // false is expected below
+  //           }, timeout - 1500); // we need time to execute the window.stop before the top level timeout hits
+  //         }),
+  //       ]);
+  //   }
+  await page.evaluate(async () => {
+    const selectors = Array.from(document.querySelectorAll("img"));
+    await Promise.all([
+      document.fonts.ready,
+      ...selectors.map((img) => {
+        // Image has already finished loading, let’s see if it worked
+        if (img.complete) {
+          // Image loaded and has presence
+          if (img.naturalHeight !== 0) return;
+          // Image failed, so it has no height
+          throw new Error("Image failed to load");
+        }
+        // Image hasn’t loaded yet, added an event listener to know when it does
+        return new Promise((resolve, reject) => {
+          img.addEventListener("load", resolve);
+          img.addEventListener("error", reject);
+        });
+      }),
+    ]);
+  });
+
+  if (response === false) {
+    console.log("Timeout rawr");
+    // timed out, resolved false
+    await page.evaluate(() => window.stop());
+  }
+
+  // let statusCode = response.status();
+  // TODO handle 4xx/5xx status codes better
+
+  let options = {
+    type: format,
+    encoding: "base64",
+    fullPage: false,
+    captureBeyondViewport: false,
+    clip: {
+      x: 0,
+      y: 0,
+      width: viewport[0],
+      height: viewport[1],
+    },
+  };
+
+  if (format === "jpeg") {
+    options.quality = 80;
+  }
+
+  let output = await page.screenshot(options);
+
+  await browser.close();
+
+  return output;
+};
